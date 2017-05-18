de8db2fdbfa7c7c1c2f08882dc3676d5908d95cc
Anu <anulayamathew@gmail.com>
2017-05-18 09:35:48 +0300

    User Page Created without file uploading

--- app/User.php
+++ app/User.php
@@ -2,8 +2,7 @@
 
 namespace App;
 
-use
-    Illuminate\Notifications\Notifiable;
+use  Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
@@ -17,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','role_id','status','photo_id'
     ];
 
     /**
